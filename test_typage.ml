a53b4541253d7a2d43fadcf292161df461acd927
Yacine Kessal <darkdims290@gmail.com>
2024-11-16 23:26:11 +0100

    partie 4 done

--- test_typage.ml
+++ test_typage.ml
@@ -1,5 +1,14 @@
 open Ast;;
 open Types;;
+
+let test_typage_entier () =
+  let env = [] in
+  let term = Int 42 in
+  match inferer_type term env 100 with
+  | Some t -> print_endline ("Type inféré pour Int 42 : " ^ print_type t)
+  | None -> print_endline "Erreur de typage pour Int 42"
+;;
+
 let test_typage_variable () =
   let env = [("x", TVar "T1")] in
   let term = Var "x" in
@@ -17,12 +26,13 @@ let test_typage_abstraction_simple () =
 ;;
 
 let test_typage_application_simple () =
-  let env = [("f", Arr(TVar "T1", TVar "T2")); ("x", TVar "T1")] in
+  let env = [("f", Arr(TVar "B1", TVar "B2")); ("x", TVar "B1")] in
   let term = App(Var "f", Var "x") in
   match inferer_type term env 100 with
   | Some t -> print_endline ("Type inféré pour App 'f x': " ^ print_type t)
   | None -> print_endline "Erreur de typage pour App 'f x'"
 ;;
+
 
 (* Test pour le typage des listes *)
 let test_typage_cons () =
@@ -50,13 +60,24 @@ let test_typage_tail () =
 ;;
 
 (* Test pour le typage du let-polymorphisme *)
-let test_typage_let_polymorphisme () =
+let test_typage_let_simple () =
   let env = [] in
   let term = Let ("id", Abs ("x", Var "x"), App (Var "id", Int 5)) in
   match inferer_type term env 100 with
+  | Some t -> print_endline ("Type inféré pour Let simple : " ^ print_type t)
+  | None -> print_endline "Erreur de typage pour Let simple"
+;;
+
+(* Test pour le typage d'une expression Let avec polymorphisme *)
+let test_typage_let_polymorphisme () =
+  let env = [] in
+  let term = Let ("id", Abs ("x", Var "x"), App (Var "id", Var "y")) in
+  let env_with_y = [("y", TVar "B")] in
+  match inferer_type term env_with_y 100 with
   | Some t -> print_endline ("Type inféré pour Let polymorphisme : " ^ print_type t)
   | None -> print_endline "Erreur de typage pour Let polymorphisme"
 ;;
+
 
 (* Test pour le typage du point fixe (Pfix) *)
 let test_typage_pfix () =
@@ -77,24 +98,25 @@ let test_typage_liste_vide () =
 ;;
 
 (* Test pour le typage d'une fonction généralisée *)
-let test_typage_forall () =
-  let env = [] in
-  let term = Abs ("x", Var "x") in
-  match inferer_type term env 100 with
-  | Some t -> print_endline ("Type inféré pour Forall : " ^ print_type t)
-  | None -> print_endline "Erreur de typage pour Forall"
+
+let test_generaliser () =
+  let env1 = [("x", TVar "A")] in
+  let ty1 = Arr (TVar "A", TVar "B") in
+  let ty_gen = generaliser ty1 env1 in
+  print_endline ("Type avant généralisation : " ^ print_type ty1);
+  print_endline ("Type après généralisation : " ^ print_type ty_gen)
 ;;
 
 
 
 let () = 
-  test_typage_variable ();
+test_typage_variable ();
   test_typage_abstraction_simple ();
   test_typage_application_simple ();
   test_typage_cons ();
   test_typage_head ();
   test_typage_tail ();
+  test_typage_let_simple ();
   test_typage_let_polymorphisme ();
   test_typage_pfix ();
   test_typage_liste_vide ();
-  test_typage_forall ();
--- types.ml
+++ types.ml
@@ -4,8 +4,7 @@ open Ast
 type ptype = 
   | TVar of string
   | Arr of ptype * ptype
-  | Nat
-  | N
+  | N of int
   | TList of ptype   
   | Forall of string list * ptype 
 ;;
@@ -15,8 +14,7 @@ let rec print_type (t : ptype) : string =
   match t with
   | TVar x -> x
   | Arr (t1,t2) -> "("^ (print_type t1)^" -> "^ (print_type t2) ^")"
-  | Nat -> "Nat"
-  | N -> "N"
+  | N x -> "N(" ^ string_of_int x ^ ")"
   | TList t -> "[" ^ (print_type t) ^ "]"
   | Forall (vars, t) ->
     let vars_str = String.concat ", " vars in
@@ -42,17 +40,28 @@ let rec cherche_type (v : string) (e : env) : ptype =
 
 (* Récupérer les variables libres qui ne sont pas dans l'environnement *)
 let rec generaliser (t : ptype) (env : env) : ptype =
+  (* Fonction pour collecter les variables de type dans un type *)
   let rec collect_vars ty acc =
     match ty with
-    | TVar x -> if List.exists (fun (v, _) -> v = x) env then acc else x :: acc
+    | TVar x -> if List.mem x acc then acc else x :: acc
     | Arr (t1, t2) -> collect_vars t1 (collect_vars t2 acc)
     | TList t -> collect_vars t acc
+    | N _ -> acc
+    | Forall (vars, t) -> 
+        let acc_filtered = List.filter (fun x -> not (List.mem x vars)) acc in
+        collect_vars t acc_filtered
     | _ -> acc
   in
+  (* Récupérer les variables de type présentes dans l'environnement *)
+  let vars_env = List.fold_left (fun acc (_, ty) -> collect_vars ty acc) [] env in
+  (* Collecter les variables libres dans le type `t` *)
   let vars_libres = collect_vars t [] in
-  if vars_libres = [] then t
-  else Forall (vars_libres, t)
+  (* Exclure les variables de l'environnement des variables libres *)
+  let vars_a_generaliser = List.filter (fun x -> not (List.mem x vars_env)) vars_libres in
+  if vars_a_generaliser = [] then t
+  else Forall (vars_a_generaliser, t)
 ;;
+
 
 let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
   match te with 
@@ -71,14 +80,14 @@ let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
       let eq_droite = genere_equa t2 ta e in
       eq_gauche @ eq_droite
   (* Question 4.2 *)
-  | Int(x) -> [(ty,N)]
-  | Add (x,y) | Sub (x,y) | Mul (x,y) ->       
-    let eq_gauche = genere_equa x N e in
-    let eq_droite = genere_equa y N e in
-    [(ty,N)] @ eq_gauche @ eq_droite
+  | Int x -> [(ty, N x)]
+  | Add (x, y) | Sub (x, y) | Mul (x, y) ->
+    let eq_gauche = genere_equa x (N 0) e in
+    let eq_droite = genere_equa y (N 0) e in
+    [(ty, N 0)] @ eq_gauche @ eq_droite
   
   | IfZero (cond, t1, t2) -> 
-    let tc = genere_equa cond N e in
+    let tc = genere_equa cond (N 0) e in
     let eq_t1 = genere_equa t1 ty e in
     let eq_t2 = genere_equa t2 ty e in
     tc @ eq_t1 @ eq_t2
@@ -89,24 +98,37 @@ let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
     let eq_t2 = genere_equa t2 ty e in
     eq_cond @ eq_t1 @ eq_t2
   
-  | Let(x,e1,e2) -> 
-    let t0 = TVar(nouvelle_var_t()) in
-    let equation_e1 = genere_equa e1 t0 e in
-    let subsitutions = unification equation_e1 [] in
-    let t0' = appliquer_substitution subsitutions t0 in 
-    let t0_gen= generaliser t0' e in
-    let env' = (x, t0_gen) :: e in
-    genere_equa e2 ty env'
+    | Let(x, e1, e2) ->
+      (* Étape 1 : Inférer le type de `e1` *)
+      let t0 = TVar(nouvelle_var_t()) in
+      let inferred_type = inferer_type e1 e 100 in
+      let t0' = match inferred_type with
+        | Some ty -> ty
+        | None -> failwith "Erreur lors de l'inférence du type de e1"
+      in
+  
+      (* Étape 2 : Généraliser le type de `e1` *)
+      let t0_gen = generaliser t0' e in
+  
+      (* Étape 3 : Ajouter `x` avec son type généralisé à l'environnement *)
+      let env' = (x, t0_gen) :: e in
+  
+      (* Étape 4 : Générer les équations pour `e2` *)
+      genere_equa e2 ty env'
+  
+
   
   | Pfix t ->
-    (* Vérification que `t` est une abstraction *)
+    (* Vérifier que `t` est une abstraction *)
     (match t with
       | Abs (x, t_body) ->
-          let ta = TVar (nouvelle_var_t()) in
-          let tr = TVar (nouvelle_var_t()) in
-          let eq_body = genere_equa t_body tr ((x, ta) :: e) in
-          (ty, Arr (ta, tr)) :: eq_body
+          let type_T = TVar (nouvelle_var_t ()) in
+          let type_U = TVar (nouvelle_var_t ()) in
+          let env' = (x, Arr (type_T, type_U)) :: e in
+          let eqs_m = genere_equa t_body (Arr (type_T, type_U)) env' in
+          (ty, Arr (type_T, type_U)) :: eqs_m
       | _ -> failwith "Pfix doit être appliqué à une abstraction")
+  
 
   | Head lst ->
     let ta = TVar (nouvelle_var_t()) in
@@ -119,10 +141,11 @@ let rec genere_equa (te : pterm) (ty : ptype) (e : env) : equa =
     (ty, TList ta) :: eq_lst
 
   | Cons (head, tail) ->
-    let ta = TVar (nouvelle_var_t()) in
-    let eq_head = genere_equa head ta e in
-    let eq_tail = genere_equa tail (TList ta) e in
-    (ty, TList ta) :: eq_head @ eq_tail
+      let ta = TVar (nouvelle_var_t()) in
+      let eq_head = genere_equa head ta e in
+      let eq_tail = genere_equa tail (TList ta) e in
+      (ty, TList ta) :: eq_head @ eq_tail
+
   
   | List xs ->
     let ta = TVar (nouvelle_var_t ()) in
@@ -142,20 +165,20 @@ and occur_check (v:string) (ty:ptype) : bool =
   | Forall (vars, t) ->
       if List.mem v vars then false
       else occur_check v t
-  | N -> false
-  | Nat -> false
+  | N _ -> false
   | _ -> false
 
 
 and substitue (v:string) (replace_with : ptype) (t:ptype) : ptype=
   match t with 
   |TVar x -> if x = v then replace_with else t 
+  | N x -> N x
   |Arr(t1,t2) -> Arr((substitue v replace_with t1),(substitue v replace_with t2))
   | TList t' -> TList (substitue v replace_with t')
   | Forall (vars, t') ->
       if List.mem v vars then t
       else Forall (vars, substitue v replace_with t')
-  | _ -> Nat
+  | _ -> failwith "dz"
 
 and substitute_in_equations (v : string) (replacement : ptype) (equations : equa) : equa =
   match equations with
@@ -178,6 +201,7 @@ and unification (equations : equa) (subs : equa) : equa =
       if t1 = t2 then unification rest subs
       else 
         match (t1, t2) with
+        | N x, N y -> unification rest subs
         | TVar x, _ -> 
             if not (occur_check x t2) then 
               let new_subs = (TVar x, t2) :: subs in 
@@ -208,48 +232,64 @@ and unification (equations : equa) (subs : equa) : equa =
 
 
 and appliquer_substitution (equations : equa) (t : ptype) : ptype =
-  match equations with
-  | [] -> t
-  | (TVar v, t') :: rest ->
-      appliquer_substitution rest (substitue v t' t)
-  | _ -> (
-      match t with
-      | Arr (t1, t2) -> Arr (appliquer_substitution equations t1, appliquer_substitution equations t2)
-      | TList t' -> TList (appliquer_substitution equations t')
-      | Forall (vars, t') ->
-          let filtered_equations = List.filter (fun (TVar x, _) -> not (List.mem x vars)) equations in
-          Forall (vars, appliquer_substitution filtered_equations t')
-      | _ -> t
+  match t with
+  | TVar v -> (
+      try
+        (* Trouver la substitution pour `TVar v` *)
+        let t' = List.assoc (TVar v) equations in
+        appliquer_substitution equations t'  (* Suivre récursivement la substitution *)
+      with Not_found -> TVar v
     )
-;;
-        
+  | Arr (t1, t2) -> Arr (appliquer_substitution equations t1, appliquer_substitution equations t2)
+  | TList t' -> TList (appliquer_substitution equations t')
+  | Forall (vars, t') ->
+      (* Appliquer les substitutions uniquement aux parties non généralisées *)
+      let filtered_equations = List.filter (fun (TVar x, _) -> not (List.mem x vars)) equations in
+      Forall (vars, appliquer_substitution filtered_equations t')
+  | _ -> t
+
 
 (* fonction qui utilise l'unification pour résoudre les equations *)
-let resoudre_equations equations limit = 
+and resoudre_equations equations limit = 
   try Some (unification equations [])
   with _ -> None
-;;
 
-let rec print_substitutions (equations : equa) =
+and trouver_variable_originale (equations : equa) (t : ptype) : ptype =
+  match t with
+  | TVar _ ->
+      (* Rechercher une clé dans les équations dont la valeur est équivalente à `t` *)
+      (try
+          List.find (fun (_, t2) -> t2 = t) equations |> fst
+        with Not_found -> t)
+  | _ -> t
+  
+and inferer_type (term : pterm) (env : env) (limit : int) : ptype option =
+  let t = TVar (nouvelle_var_t()) in
+  let equations = genere_equa term t env in
+  (* print_endline "=== Équations générées ===";
+  print_equations equations; *)
+  match resoudre_equations equations limit with
+  | None -> print_endline "Échec de l'unification des équations"; None
+  | Some eqs -> 
+    (* print_endline "=== Substitutions appliquées ===";
+    print_substitutions eqs; *)
+    let final_type = appliquer_substitution eqs t in
+    let resolved_type = appliquer_substitution eqs final_type in
+    let fully_resolved_type = appliquer_substitution eqs resolved_type in
+    let variable_originale = trouver_variable_originale eqs fully_resolved_type in
+    Some variable_originale
+
+
+
+and  print_substitutions (equations : equa) =
   List.iter (fun (t1, t2) -> print_endline (print_type t1 ^ " = " ^ print_type t2)) equations
-;;
 
-let print_equation (e1, e2) =
+and print_equation (e1, e2) =
   print_endline ("(" ^ print_type e1 ^ " = " ^ print_type e2 ^ ")")
-;;
 
-let rec print_equations equations =
+and print_equations equations =
   match equations with
   | [] -> ()
   | eq :: rest -> print_equation eq; print_equations rest
 ;;
 
-let inferer_type (term : pterm) (env : env) (limit : int) : ptype option =
-  let t = TVar (nouvelle_var_t()) in
-  let equations = genere_equa term t env in
-  match resoudre_equations equations limit with
-  | None -> print_endline "echec de l'unification des equations"; None
-  | Some eqs -> 
-    let final_type = appliquer_substitution eqs t in
-    Some final_type
-;;
